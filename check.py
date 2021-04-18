c7656208eaebe1dce3b330fac0a1540001bba1a2
larsnyborgpedersen <lnpe17@student.aau.dk>
2021-04-18 14:03:54 +0200

    Now it can also check if there are too many zero's in total

--- check.py
+++ check.py
@@ -11,41 +11,58 @@ for subdir, dirs, files in os.walk(rootdir):
             with open(os.path.join(subdir, file), newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
-                Delta_TP9 = 0
-                Delta_AF7 = 0
-                Delta_AF8 = 0
-                Delta_TP10 = 0
-                tooManyZeros = False
+                Delta_TP9_zero_in_a_row = 0
+                Delta_AF7_zero_in_a_row = 0
+                Delta_AF8_zero_in_a_row = 0
+                Delta_TP10_zero_in_a_row = 0
+                too_may_zero_in_a_row = False
+
+                Delta_TP9_zero_total = 0
+                Delta_AF7_zero_total = 0
+                Delta_AF8_zero_total = 0
+                Delta_TP10_zero_total = 0
+                too_many_zero_total = False
 
                 incrementTEMP = 0
 
 
                 for row in reader:
                     if (row[1] == "0.0"):
-                        Delta_TP9 += 1
+                        Delta_TP9_zero_in_a_row += 1
+                        Delta_TP9_zero_total += 1
+
                     else:
-                        Delta_TP9 = 0
+                        Delta_TP9_zero_in_a_row = 0
 
                     if (row[1] == "0.0"):
-                        Delta_AF7 += 1
+                        Delta_AF7_zero_in_a_row += 1
+                        Delta_AF7_zero_total += 1
                     else:
-                        Delta_AF7 = 0
+                        Delta_AF7_zero_in_a_row = 0
 
                     if (row[1] == "0.0"):
-                        Delta_AF8 += 1
+                        Delta_AF8_zero_in_a_row += 1
+                        Delta_AF8_zero_total += 1
                     else:
-                        Delta_AF8 = 0
+                        Delta_AF8_zero_in_a_row = 0
 
                     if (row[1] == "0.0"):
-                        Delta_TP10 += 1
+                        Delta_TP10_zero_in_a_row += 1
+                        Delta_TP10_zero_total += 1
                     else:
-                        Delta_TP10 = 0
+                        Delta_TP10_zero_in_a_row = 0
 
-                    tooMany = 1000
-                    if Delta_TP9 > tooMany or Delta_AF7 > tooMany or Delta_AF8 > tooMany or Delta_TP10 > tooMany:
-                       tooManyZeros = True
+                    too_many_in_a_row = 100
+                    if Delta_TP9_zero_in_a_row > too_many_in_a_row or Delta_AF7_zero_in_a_row > too_many_in_a_row or Delta_AF8_zero_in_a_row > too_many_in_a_row or Delta_TP10_zero_in_a_row > too_many_in_a_row:
+                       too_may_zero_in_a_row = True
 
+                    too_many_total = 1000
+                    if Delta_TP9_zero_total > too_many_total or Delta_AF7_zero_total > too_many_total or Delta_AF8_zero_total > too_many_total or Delta_TP10_zero_total > too_many_total:
+                        too_many_zero_total = True
                     #print(', '.join(row))
 
-                if (tooManyZeros):
-                    print("Too many zero's in file: " + os.path.join(subdir, file))
+                if (too_may_zero_in_a_row):
+                    print("Too many zero's IN A ROW in file: " + os.path.join(subdir, file))
+                if (too_many_zero_total):
+                    print("Too many zero's TOTAL in file: " + os.path.join(subdir, file))
+
